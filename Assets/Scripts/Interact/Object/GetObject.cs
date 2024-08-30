040438ca384fe157ed2906661082323aed6a7eb2
meo-young <meoyoung000@naver.com>
2024-08-30 20:07:12 +0900

    인벤토리 로직 수정

--- Assets/Scripts/Interact/Object/GetObject.cs
+++ Assets/Scripts/Interact/Object/GetObject.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static ItemInfo;
 
 public class GetObject : MonoBehaviour, IInteractable
 {
     [Tooltip("아이템을 추가할 인벤토리")]
     [SerializeField] private InventoryManager inventoryManager;
     [Tooltip("아이템 이름")]
-    [SerializeField] private string itemName;
+    [SerializeField] private Item itemInfo;
 
     private void Start()
     {
@@ -19,7 +20,7 @@ public class GetObject : MonoBehaviour, IInteractable
     //인벤토리에 아이템을 추가한 후 필드에서 아이템 삭제
     public void Interact()
     {
-        inventoryManager.AddItem(itemName);
+        inventoryManager.AddItem(itemInfo);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/Player/Inventory/ItemInfo.cs
+++ Assets/Scripts/Player/Inventory/ItemInfo.cs
@@ -1,18 +1,13 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Item
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Carrot,
+        Potato,
+        Tomato
     }
 }
