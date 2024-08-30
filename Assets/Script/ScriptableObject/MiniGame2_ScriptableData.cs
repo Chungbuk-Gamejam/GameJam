323d31cbaa0307398f5ea4a3a3d5d022483e672e
sin15348@naver.com <sin15348@naver.com>
2024-08-31 03:24:53 +0900

    미니게임 2_추가

---
+++ Assets/Script/ScriptableObject/MiniGame2_ScriptableData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class MiniGame2_ScriptableData : ScriptableObject
+{
+    public List<MiniGame2_ScriptableDetailData> _listData;
+}
---
+++ Assets/Script/ScriptableObject/MiniGame2_ScriptableDetailData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class MiniGame2_ScriptableDetailData : ScriptableObject
+{
+    public int id;
+
+    public Vector3 _pos1;
+    public Vector3 _pos2;
+    public Vector3 _pos3;
+}
