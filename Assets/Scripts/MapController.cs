49aafb6ac14f6a015483a35e4c94e76fcd4e2e8c
meo-young <meoyoung000@naver.com>
2024-09-01 02:02:56 +0900

    깃헙 복구

---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapController : MonoBehaviour
+{
+    [Header("¸Ê")]
+    [SerializeField] GameObject map;
+    [SerializeField] List<TMP_Text> region;
+    [SerializeField] int counter = 0;
+    [SerializeField] PlayerController playerController;
+
+    [Header("Ã»È­")]
+    [SerializeField] List<Sprite> sprites;
+    [SerializeField] List<Sprite> characterSprites;
+    [SerializeField] Image character;
+    [SerializeField] Image chunghwaImage;
+    [SerializeField] GameObject chunghwa;
+
+    private bool isActivated = false;
+
+    private void OnEnable()
+    {
+        isActivated = false;
+        counter = 0;
+    }
+    private void Update()
+    {
+        switch (counter)
+        {
+            case 0:
+                SetFontSize(0);
+                break;
+            case 1:
+                SetFontSize(1);
+                break;
+            case 2:
+                SetFontSize(2);
+                break;
+            case 3:
+                SetFontSize(3);
+                break;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (counter >= region.Count - 1)
+            {
+                counter = 0;
+            }
+            else
+            {
+                counter++;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (counter == 0)
+            {
+                counter = region.Count - 1;
+            }
+            else
+            {
+                counter--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isActivated)
+            {
+                if (ProjectManager.Instance.isLocked)
+                {
+                    if(counter == 0)
+                    {
+                        isActivated = true;
+                        playerController.dayCount = counter + 1;
+                        StartCoroutine(StartChangeScene());
+                    }
+                }
+                else
+                {
+                    isActivated = true;
+                    playerController.dayCount = counter + 1;
+                    StartCoroutine(StartChangeScene());
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ProjectManager.Instance.UnLock();
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            ProjectManager.Instance.Lock();
+        }
+    }
+
+    IEnumerator StartChangeScene()
+    {
+        yield return new WaitForSeconds(1.0f);
+        FadeManager.Instance.StartFadeIn();
+        yield return new WaitForSeconds(1.5f);
+        if (playerController.cookCount == 1)
+        {
+            character.sprite = characterSprites[0];
+        }
+        else if (playerController.cookCount == 2)
+        {
+            character.sprite = characterSprites[1];
+        }
+        else if (playerController.cookCount == 3)
+        {
+            character.sprite = characterSprites[2];
+        }
+
+        chunghwaImage.sprite = sprites[counter];
+        playerController.dayCount++;
+
+        chunghwa.SetActive(true);
+        FadeManager.Instance.StartFadeOut();
+
+        yield return new WaitForSeconds(5.0f);
+        FadeManager.Instance.StartFadeIn();
+        yield return new WaitForSeconds(1.5f);
+        playerController.gameObject.transform.position = playerController._transform.position;
+        chunghwa.SetActive(false);
+        FadeManager.Instance.StartFadeOut();
+        playerController.ChangeState(playerController._idleState);
+        map.SetActive(false);
+    }
+
+    private void SetFontSize(int focus)
+    {
+        for (int i = 0; i < region.Count; i++)
+        {
+            if (i == focus)
+            {
+                region[i].fontSize = 42;
+            }
+            else
+            {
+                region[i].fontSize = 36;
+            }
+        }
+    }
+
+}
