7bc1884e651ec5e0debf24ec55bec46150cb217f
sin15348@naver.com <sin15348@naver.com>
2024-08-31 15:44:45 +0900

    미니게임 1번 _ 스프라이트 추가

--- Assets/Script/MiniGameObject.cs
+++ Assets/Script/MiniGameObject.cs
@@ -61,12 +61,14 @@ public class MiniGameObject : MonoBehaviour
     {
         _onCompleteGame?.Invoke(true);
         _onCompleteGame = null;
+        Debug.Log($"SuccessGame");
     }
 
     public void FailGame()
     {
         _onCompleteGame?.Invoke(false);
         _onCompleteGame = null;
+        Debug.Log($"FailGame");
     }
 
     public bool CheckisSuccess()
@@ -74,9 +76,9 @@ public class MiniGameObject : MonoBehaviour
         Vector3 vDown = Vector3.down;
         Vector3 vOrigin = Vector3.zero;
         if (_point != null)
-            vOrigin = _point.transform.position;
+            vOrigin = _point.GetComponent<RectTransform>().position;
 
-        RaycastHit2D hit = Physics2D.Raycast(vOrigin, Vector3.down, 15, _layerMask);
+        RaycastHit2D hit = Physics2D.Raycast(vOrigin, Vector3.down, 300);
         if (hit.collider != null)
             return true;
 
@@ -88,7 +90,7 @@ public class MiniGameObject : MonoBehaviour
         float pointPositionX = 0f;
 
         if (_point != null)
-            pointPositionX = _point.transform.position.x;
+            pointPositionX = _point.GetComponent<RectTransform>().localPosition.x;
 
         if (_MoveRight == true && _moveLimit < pointPositionX)
             _MoveRight = false;
@@ -100,11 +102,11 @@ public class MiniGameObject : MonoBehaviour
 
         if (_point != null)
         {
-            Vector3 P0 = _point.transform.position;
+            Vector3 P0 = _point.GetComponent<RectTransform>().localPosition;
             Vector3 AT = dir * _speed * Time.deltaTime;
             Vector3 P1 = P0 + AT;
 
-            _point.transform.position = P1;
+            _point.GetComponent<RectTransform>().localPosition = P1;
             // 등속운동 P1 = PO + AT;
         }
     }
@@ -114,8 +116,10 @@ public class MiniGameObject : MonoBehaviour
         if(_point!= null )
         {
             Color color = Color.magenta;
-            Vector3 origin = _point.transform.position;
-            Vector3 to = _point.transform.position + (Vector3.down * 15f);
+            Vector2 origin = _point.GetComponent<RectTransform>().position;
+            Vector2 to = new Vector2
+                (_point.GetComponent<RectTransform>().position.x,
+                _point.GetComponent<RectTransform>().position.y + (-1f * 300f));
 
             Gizmos.DrawLine(origin, to);
         }
