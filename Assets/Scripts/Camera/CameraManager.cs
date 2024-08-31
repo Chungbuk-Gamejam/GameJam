84d652ce563155bbf799dec576464442cc6f9c8f
meo-young <meoyoung000@naver.com>
2024-09-01 05:54:27 +0900

    레시피 중복 출력 수정

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -16,6 +16,10 @@ public class CameraManager : MonoBehaviour
     [Tooltip("카메라가 움직일 수 있는 영역")]
     [SerializeField] private BoxCollider2D currentBound;
 
+    private Vector3 originalPosition;
+    private float originalSize;
+    public BoxCollider2D targetArea; // 카메라가 비춰야 할 영역
+
     private Vector3 minBound;
     private Vector3 maxBound;
 
@@ -23,6 +27,8 @@ public class CameraManager : MonoBehaviour
     private float halfHeight;
 
     private Camera theCamera;
+
+    private bool isActivated = false;
    
     void Start()
     {
@@ -33,11 +39,14 @@ public class CameraManager : MonoBehaviour
         maxBound = currentBound.bounds.max;
         halfHeight = theCamera.orthographicSize;
         halfWidth = halfHeight * Screen.width / Screen.height;
+
+        originalPosition = theCamera.transform.position;
+        originalSize = theCamera.orthographicSize;
     }
 
     void LateUpdate()
     {
-        if (target != null)
+        if (target != null && !isActivated)
         {
             UpdateCurrentBound();
             UpdateCameraPosition();
@@ -80,5 +89,32 @@ public class CameraManager : MonoBehaviour
         currentBound = newBound;
         minBound = currentBound.bounds.min;
         maxBound = currentBound.bounds.max;
+    }
+
+    public void AdjustCameraToArea()
+    {
+        isActivated = true;
+        // BoxCollider2D의 크기에 맞게 카메라 위치 조정
+        Bounds bounds = targetArea.bounds;
+        theCamera.transform.position = new Vector3(bounds.center.x, bounds.center.y, theCamera.transform.position.z);
+
+        // 카메라의 orthographicSize를 박스 콜라이더의 높이에 맞춰 조정
+        float aspectRatio = (float)Screen.width / Screen.height;
+        theCamera.orthographicSize = bounds.size.y / 2;
+
+        // 카메라의 너비가 영역을 벗어나지 않도록 조정
+        float cameraWidth = theCamera.orthographicSize * aspectRatio;
+        if (cameraWidth > bounds.size.x / 2)
+        {
+            theCamera.orthographicSize = bounds.size.x / 2 / aspectRatio;
+        }
+    }
+
+    public void ResetCamera()
+    {
+        isActivated = false;
+        // 카메라를 원래 위치와 사이즈로 복구
+        theCamera.transform.position = originalPosition;
+        theCamera.orthographicSize = originalSize;
     }
 }
