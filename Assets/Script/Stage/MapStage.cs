9df87d5108cfdad68d61f96ecb6cdf26f3d8fb2e
jonghow <sin15348@naver.com>
2024-09-01 11:10:14 +0900

    씬 수정, 솔방울게임 rotation freeze z

--- Assets/Script/Stage/MapStage.cs
+++ Assets/Script/Stage/MapStage.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class MapStage : MonoBehaviour
 {
+    PlayerController _player;
     public void Start()
     {
         SoundManager.instance.TurnOffBackGroundMusic();
         SoundManager.instance.PlayBackgroundMusicForMainGame();
     }
+
+    public void Update()
+    {
+        if(_player != null)
+        {
+
+        }
+    }
+
+
+
+
 }
